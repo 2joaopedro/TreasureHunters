899a1a19e0653ccf39db0edc9c9a926c8e46ee42
JoãoPedro <70731779+2joaopedro@users.noreply.github.com>
2024-10-10 10:38:41 -0300

    Fiz a Movimentação do personagem já com animação

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rig;
+    private Animator anim;
+    public float speed;
+    private SpriteRenderer sprite;
+    private Vector2 direction;
+    private Vector2 lastDirection = Vector2.zero;
+
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+      direction = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+       
+        if(direction.magnitude > 0f) 
+        {
+            lastDirection = direction;
+            if(direction.x > 0f)
+            {
+                sprite.flipX = false;
+                anim.SetInteger("Base", 1);     
+            } 
+            else if (direction.x < 0f) 
+            {
+                sprite.flipX = true;
+                anim.SetInteger("Base", 1);           
+            } 
+        }
+        else 
+        {
+            if(lastDirection.x > 0f)
+            {
+                sprite.flipX = false;
+                anim.SetInteger("Base", 0);     
+            } 
+            else if (lastDirection.x < 0f) 
+            {
+                sprite.flipX = true;
+                anim.SetInteger("Base", 0);           
+            } 
+        }
+    }
+
+    void FixedUpdate() {
+        rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
     }
 }
